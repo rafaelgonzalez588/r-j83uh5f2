e70af42595d1b76cbe7decfcc4a5df5d5af612a6
RafaelGonzalez <rafaelgonzalez588@gmail.com>
2020-05-04 21:15:49 -0500

    reto cumplido

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    contador: 0,
+  };
+  cuenta = (event) => {
+    this.setState({
+      contador: event.target.value.length
+    });
+  };
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.cuenta}></textarea>
+        <div className="counter">{this.state.contador}</div>
       </div>
     );
   }
